337c953dfbcbcb8aa7444ee60729a5e4868723b3
Katarzyna Kraus <katarzynakraus@Admins-MacBook-Pro-4.local>
2016-02-21 14:34:20 +0100

    finish uniq function

--- cart/lib/cart.ex
+++ cart/lib/cart.ex
@@ -1,2 +1,22 @@
 defmodule Cart do
+  def add([],{}) do
+   []
+ end
+ def add(alist, action) do
+   alist ++ [action]
+ end
+
+ def show([]) do
+   []
+ end
+
+ def unique([]) do
+  []
+ end
+def unique(alist) do
+Enum.map(alist, fn {_, item} -> item end) |>
+Enum.uniq
+
+end
+
 end
--- cart/test/cart_test.exs
+++ cart/test/cart_test.exs
@@ -2,7 +2,21 @@ defmodule CartTest do
   use ExUnit.Case
   doctest Cart
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "add_to_history" do
+    assert Cart.add([], {})==[]
+    assert Cart.add([], {:+, 'banana'})==[{:+, 'banana'}]
+    assert Cart.add([{:+, 'banana'}], {:+, 'apple'})==[{:+, 'banana'}, {:+, 'apple'}]
+    assert Cart.add([{:+, 'banana'}], {:-, 'banana'})==[{:+, 'banana'}, {:-, 'banana'}]
   end
+
+  test "show cart" do
+    assert Cart.show([]) == []
+  end
+
+  test "show unique" do
+    assert Cart.unique([]) == []
+    assert Cart.unique([{:+,'banana'}]) == ['banana']
+    assert Cart.unique([{:+,'banana'}, {:+,'banana'}]) == ['banana']
+  end
+
 end
